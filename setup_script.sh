71d708ad8d16f7922f9615695cc8f5a3d0c1c11d
Tereza Kulovana <teri.kulovana@gmail.com>
2019-03-13 10:15:48 +0100

    Clone git repository and copy files from it. (see #8)

--- setup_script.sh
+++ setup_script.sh
@@ -26,18 +26,21 @@ pip install django_python3_ldap
 django-admin startproject web_console_project .
 python manage.py startapp users
 
+# clone github respository
+git clone https://github.com/ctu-geoforall-lab-projects/dp-kulovana-2019.git $PROJ_PATH/dp-kulovana-2019 
+
 # copy files
-cp -r $PROJ_PATH/../dp-kulovana-2019/src/templates $PROJ_PATH/templates
+cp -r $PROJ_PATH/dp-kulovana-2019/src/templates $PROJ_PATH/templates
 
-cp $PROJ_PATH/../dp-kulovana-2019/src/users/admin.py $PROJ_PATH/users/admin.py
-cp $PROJ_PATH/../dp-kulovana-2019/src/users/forms.py $PROJ_PATH/users/forms.py
-cp $PROJ_PATH/../dp-kulovana-2019/src/users/models.py $PROJ_PATH/users/models.py
-cp $PROJ_PATH/../dp-kulovana-2019/src/users/urls.py $PROJ_PATH/users/urls.py
-cp $PROJ_PATH/../dp-kulovana-2019/src/users/views.py $PROJ_PATH/users/views.py
+cp $PROJ_PATH/dp-kulovana-2019/src/users/admin.py $PROJ_PATH/users/admin.py
+cp $PROJ_PATH/dp-kulovana-2019/src/users/forms.py $PROJ_PATH/users/forms.py
+cp $PROJ_PATH/dp-kulovana-2019/src/users/models.py $PROJ_PATH/users/models.py
+cp $PROJ_PATH/dp-kulovana-2019/src/users/urls.py $PROJ_PATH/users/urls.py
+cp $PROJ_PATH/dp-kulovana-2019/src/users/views.py $PROJ_PATH/users/views.py
 
-cp $PROJ_PATH/../dp-kulovana-2019/src/project/settings.py $PROJ_PATH/web_console_project/settings.py
-cp $PROJ_PATH/../dp-kulovana-2019/src/project/settings_custom.py $PROJ_PATH/web_console_project/settings_custom.py
-cp $PROJ_PATH/../dp-kulovana-2019/src/project/urls.py $PROJ_PATH/web_console_project/urls.py
+cp $PROJ_PATH/dp-kulovana-2019/src/project/settings.py $PROJ_PATH/web_console_project/settings.py
+cp $PROJ_PATH/dp-kulovana-2019/src/project/settings_custom.py $PROJ_PATH/web_console_project/settings_custom.py
+cp $PROJ_PATH/dp-kulovana-2019/src/project/urls.py $PROJ_PATH/web_console_project/urls.py
 
 # migrate db
 python manage.py makemigrations users
