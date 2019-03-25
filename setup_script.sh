025de622f1b06fd5127278159e324836266cc20c
Tereza Kulovana <teri.kulovana@gmail.com>
2019-03-25 12:37:46 +0100

    During setup: copy repository to temporary files and then delete it.

--- setup_script.sh
+++ setup_script.sh
@@ -27,23 +27,26 @@ django-admin --version
 django-admin startproject web_console_project .
 python manage.py startapp users
 
-# clone github respository
-git clone https://github.com/ctu-geoforall-lab-projects/dp-kulovana-2019.git $PROJ_PATH/dp-kulovana-2019 
+# clone github repository
+git clone https://github.com/ctu-geoforall-lab-projects/dp-kulovana-2019.git /tmp/dp-kulovana-2019
 
 # copy files
-cp -r $PROJ_PATH/dp-kulovana-2019/src/templates $PROJ_PATH/templates
+cp -r /tmp/dp-kulovana-2019/src/templates $PROJ_PATH/templates
 
-cp $PROJ_PATH/dp-kulovana-2019/src/users/admin.py $PROJ_PATH/users/admin.py
-cp $PROJ_PATH/dp-kulovana-2019/src/users/forms.py $PROJ_PATH/users/forms.py
-cp $PROJ_PATH/dp-kulovana-2019/src/users/models.py $PROJ_PATH/users/models.py
-cp $PROJ_PATH/dp-kulovana-2019/src/users/urls.py $PROJ_PATH/users/urls.py
-cp $PROJ_PATH/dp-kulovana-2019/src/users/views.py $PROJ_PATH/users/views.py
-cp $PROJ_PATH/dp-kulovana-2019/src/users/templatetags $PROJ_PATH/users/templatetags
+cp /tmp/dp-kulovana-2019/src/users/admin.py $PROJ_PATH/users/admin.py
+cp /tmp/dp-kulovana-2019/src/users/forms.py $PROJ_PATH/users/forms.py
+cp /tmp/dp-kulovana-2019/src/users/models.py $PROJ_PATH/users/models.py
+cp /tmp/dp-kulovana-2019/src/users/urls.py $PROJ_PATH/users/urls.py
+cp /tmp/dp-kulovana-2019/src/users/views.py $PROJ_PATH/users/views.py
+cp -r /tmp/dp-kulovana-2019/src/users/templatetags $PROJ_PATH/users/templatetags
 
-cp $PROJ_PATH/dp-kulovana-2019/src/project/settings.py $PROJ_PATH/web_console_project/settings.py
-cp $PROJ_PATH/dp-kulovana-2019/src/project/settings_custom.py $PROJ_PATH/web_console_project/settings_custom.py
-cp $PROJ_PATH/dp-kulovana-2019/src/project/urls.py $PROJ_PATH/web_console_project/urls.py
-cp $PROJ_PATH/dp-kulovana-2019/src/project/ldap_auth.py $PROJ_PATH/web_console_project/ldap_auth.py
+cp /tmp/dp-kulovana-2019/src/project/settings.py $PROJ_PATH/web_console_project/settings.py
+cp /tmp/dp-kulovana-2019/src/project/settings_custom.py $PROJ_PATH/web_console_project/settings_custom.py
+cp /tmp/dp-kulovana-2019/src/project/urls.py $PROJ_PATH/web_console_project/urls.py
+cp /tmp/dp-kulovana-2019/src/project/ldap_auth.py $PROJ_PATH/web_console_project/ldap_auth.py
+
+# delete github repository
+rm -rf /tmp/dp-kulovana-2019
 
 # migrate db
 python manage.py makemigrations users
