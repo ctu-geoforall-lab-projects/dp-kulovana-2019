6661e586131764606c1fd459595920a5fa98d9c6
Tereza Kulovana <teri.kulovana@gmail.com>
2019-04-14 12:59:21 +0000

    Propagate adding/removing groups into LDAP. Sync superuser status. (see #17 #18)

--- src/project/ldap_sync.py
+++ src/project/ldap_sync.py
@@ -2,7 +2,6 @@ from django_python3_ldap import ldap
 import ldap3
 from django_python3_ldap.conf import settings
 from django.contrib.auth.models import Group
-
 import logging
 logger = logging.getLogger('django')
 
@@ -54,6 +53,40 @@ class SyncDjangoLDAP():
             self._connection.modify(f'uid={obj.username},ou=People,dc=gis,dc=lab',
                 {'description': [(ldap3.MODIFY_REPLACE, [f'{obj.description}'])]})
             logger.info(f'Account {obj.username} updated with description {obj.description}')
+        if 'groups' in form.changed_data:
+            groups_django = Group.objects.all()
+            for one_group_django in groups_django:
+                # check if user is in Django group
+                django_group = obj.groups.filter(name=f'{one_group_django}').exists()
+                # check if user is in the same LDAP group
+                SEARCH_BASE_GROUPS = "ou=groups,dc=gis,dc=lab"
+                CUSTOM_SEARCH_FILTER = f'(&(ObjectClass=posixGroup)(cn={one_group_django})(memberUid={obj.username}))'
+                ldap_group = self._connection.search(
+                    search_base = SEARCH_BASE_GROUPS,
+                    search_filter = CUSTOM_SEARCH_FILTER,
+                    attributes=ldap3.ALL_ATTRIBUTES,
+                )
+
+                # user in Django and not in LDAP
+                if django_group and not ldap_group:
+                    self._connection.modify(f'cn={one_group_django},ou=Groups,dc=gis,dc=lab',
+                        {'memberUid': [(ldap3.MODIFY_ADD, [f'{obj.username}'])]})
+                    logger.info(f'Account {obj.username} was added to group {one_group_django}')
+
+                # user not in Django and in LDAP
+                elif not django_group and ldap_group:
+                    self._connection.modify(f'cn={one_group_django},ou=Groups,dc=gis,dc=lab',
+                        {'memberUid': [(ldap3.MODIFY_DELETE, [f'{obj.username}'])]})
+                    logger.info(f'Account {obj.username} was deleted from group {one_group_django}')
+
+                # add superuser status respectively to gislabadmins status
+                if one_group_django.name == 'gislabadmins':
+                    obj.is_staff = django_group
+                    obj.is_superuser = django_group
+                    obj.save()
+                    if django_group:
+                        logger.info(f'User {obj.username} is a superuser')
+
 
     def save_user(self, obj, form):
         logger.info('save_user function')
